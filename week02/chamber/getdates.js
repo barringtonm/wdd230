753254b989f93e22b5c79a0af46c1f7986a27128
Barrington Miller <barrington.miller@myyahoo.com>
2024-05-17 01:54:51 -0400

    form files

---
+++ week02/chamber/getdates.js
@@ -0,0 +1,5 @@
+let text;
+text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+/* Date.now() timestamp*/
+document.querySelector('.today').textContent = Date.now();
